4fd593ed91f42fc49dc3fd8369be7349a0c365b0
mateussgont <mateussilvagontijo@gmail.com>
2024-09-15 15:35:49 -0300

    test

--- PA2/lextest.d
+++ PA2/lextest.d
@@ -1,5 +1,4 @@
-lextest.o lextest.d : lextest.cc /var/tmp/cool/include/PA2/copyright.h \
- /var/tmp/cool/include/PA2/cool-parse.h \
+lextest.o lextest.d : lextest.cc /var/tmp/cool/include/PA2/cool-parse.h \
  /var/tmp/cool/include/PA2/copyright.h \
  /var/tmp/cool/include/PA2/cool-io.h /var/tmp/cool/include/PA2/tree.h \
  /var/tmp/cool/include/PA2/stringtab.h /var/tmp/cool/include/PA2/list.h \
